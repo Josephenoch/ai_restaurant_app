87b769fbb9e86ccf601e4fbf34d3e5b46d8c167a
Joseph Enoch <enochjoseph01@gmail.com>
2024-09-24 20:16:44 +0100

    chore: complete the app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,5 @@
-import { Tabs, Stack } from 'expo-router';
+import {Stack } from 'expo-router';
 import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
